c45d055b4ef071b1ecad4a9808f6d3a2d34a1b11
shih002581 <d11416147@gs.takming.edu.tw>
2026-04-07 15:42:29 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Http.Headers;
+
+class Person
+{
+    public string name;
+    public int Id;
+    public Person(int id, string name)
+    {
+        Id = id;
+        this.name = name;
+    }
+    public string GetName()
+    {
+        return "Person的Name";
+    }
+
+    public virtual string Info()
+    {
+        return "Person的info";
+    }
+}
+class Student : Person
+{
+    private int Score;
+    public Student(int id, string name, int score) : base(id, name)
+    {
+        Score = score;
+    }
+    public new string GetName()
+    {
+        return "學生的Name";
+    }
+    public override string Info()
+    {
+        return "學生的Info";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Class_0407_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student(1234, "咚咚紐", 98);
+            Console.WriteLine(s.GetName());
+            Console.WriteLine(s.Info());
+        }
+    }
+}
